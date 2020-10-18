4e17bc166831a5380aba1f07fe103e22037cbb4c
Ina Botnaru <inabotnaru1@gmail.com>
2020-10-18 14:07:40 +0300

    task limit

--- main.rb
+++ main.rb
@@ -9,48 +9,57 @@ require 'D:\UTM\2020\PAD\lab1\jobs\deliver.rb'
 
 Mongoid.load!(File.join(File.dirname(__FILE__), 'config', 'mongoid.yml'))
 
+LIMIT = 8 #task limit
+
   get '/orders' do
     Coffee.all.to_json
   end
 
-  get '/orders/:id' do
+  get '/orders/:id' do #gets a specific order 
     order_id = params['id']
     order = Coffee.find(order_id)
     json(order)
   end
 
-  post '/orders' do
+  post '/orders' do #create a new order based on request, if the task limit isn't reached
+    if !can_create_order 
+      halt 400, json({error:"Task limit reached"})
+    end
     body = JSON.parse(request.body.read)
     coffee_order = Coffee.create(vegan:body["vegan"], type:body["type"], sugar_cubes:body["sugar_cubes"], status:"ordered")
     json(coffee_order)
   end
 
-  put '/orders/:id' do
+  put '/orders/:id' do #the order is started and based on coffee type the time for preparation is calculated
     order_id = params["id"]
     order = Coffee.find(order_id)
     order.status = "starting"
     order.save
-      
+    
     Deliver.perform_in(time_for_preparation(order.type),{id:order_id})
-
     json(order)
   end
 
-  get '/status' do
+  get '/status' do #gets all teh orders that are being processed
     count = Coffee.all.to_a.count{|order|["ordered","starting"].include?(order.status)}
     json({count:count})
   end
 
 
   def time_for_preparation (coffe_type)
-    if coffee_type = "americano" 
+    if coffee_type == "americano" 
       return 15 
-    elsif coffe_type = "espresso"
+    elsif coffe_type == "espresso"
       return 3
-    elsif coffe_type = "latte"
+    elsif coffe_type == "latte"
       return 7
     else return 40
     end
+  end
+
+  def can_create_order
+    count = Coffee.all.to_a.count{|order|["ordered","starting"].include?(order.status)}
+    count < LIMIT
   end
 
 
